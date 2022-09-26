66b92e389c617d9195bec39f0c8311c12eb6625f
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-09-26 19:05:57 +0700

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,7 +6,7 @@ let name = await conn.getName(who)
 
 
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Maxxy;Bot;;Md\nFN:Saya Owner Maxxy Bot WhatsApp, Md\nNICKNAME:👑 Owner Maxxy Bot\nORG:Wudy\nTITLE:soft\nitem1.TEL;waid=6285380166282:+62 853-8016-6282\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://s.id/Cerdasin62\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:kahfilangsa@mail.com\nitem3.X-ABLabel:💌 Mail Owner 𝑴𝒂𝒂𝒙𝒚 𝑩𝒐𝒕𝒛\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 13 January 2001\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Vanessa;Bot;;Md\nFN:Saya Owner Vanessa Bot WhatsApp, Md\nNICKNAME: Owner Vanessa Bot\nORG:Wudy\nTITLE:soft\nitem1.TEL;waid=6287889347327\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://s.id/1iYtC\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:esacaesar17@gmail.com\nitem3.X-ABLabel:💌 Mail Owner Vanessa\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 7 October 2008\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
@@ -18,13 +18,13 @@ await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard
 if (command == 'creator') {
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `kahfilangsa@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://github.com/Kahfi-XD`, `👤 Gada pawang nih senggol dong 😔`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://s.id/Cerdasin62/`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me`, `esacaesar17@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://github.com/DreamLikeAmonster`, `👤 Gada pawang nih senggol dong :v`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://s.id/1iYtC/`, `🤖 Hanya bot biasa yang kadang suka eror:V`]
   ], fakes)
-  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
+  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya`, sentMsg, {mentions: [m.sender]})
   } catch {
   const sentMsg = await conn.sendContact(m.chat, `${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, m)
-  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, sentMsg, {mentions: [m.sender]})
+  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor team developerku, jangan di apa-apain bg:v`, sentMsg, {mentions: [m.sender]})
   }
   }
 }
