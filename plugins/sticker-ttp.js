12de18dc6693d53b4c1ff33bb2ef994561fddaf6
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-11-08 07:24:18 +0700

    Update sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, text }) => {
     let teks = m.quoted ? m.quoted.text : text
     let links
     try {
-    links = global.API('xteam', '/ttp', { file: '', text: teks })
+    links = global.API('vanessaa', '/ttp', { file: '', text: teks })
     } catch {
-    links = global.API(`https://anabotofc.herokuapp.com/api/maker/ttp?apikey=AnaBot&text=${teks}`)
+    links = global.API(`https://vanessaa-rest-api.herokuapp.com/api/search/googleimage?text=${text}`)
     }
     let stiker = await sticker(null, links, global.packname, global.author)
     if (stiker) await conn.sendFile(m.chat, stiker, 'sticker.webp', '', fakes, null, adReply)
