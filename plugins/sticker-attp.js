d34ad56da066661bba791438c66fd73432367ba2
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-11-22 05:52:32 +0700

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,35 +1,8 @@
 let handler = async (m, { conn, text }) => {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    try {
-    conn.sendFile(m.chat, global.API('vanessaa', '/attp', { file: '', text: teks }), 'attp.webp', '', fakes, null, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: { externalAdReply :{
-    showAdAttribution: true,
-    mediaUrl: sfb,
-    mediaType: 2,
-    description: wm,
-    title: 'Hai, ' + ucapan,
-    body: botdate,
-    thumbnail: tumhiho,
-    sourceUrl: sfb
-     }}
-  })
-    } catch {
-    conn.sendFile(m.chat, global.API(`https://vanessaa-rest-api.herokuapp.com/api/maker/attp?text=${text}`), 'attp.webp', '', fakes, null, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: { externalAdReply :{
-    showAdAttribution: true,
-    mediaUrl: sfb,
-    mediaType: 2,
-    description: wm,
-    title: 'Hai, ' + ucapan,
-    body: botdate,
-    thumbnail: tumhiho,
-    sourceUrl: sfb
-     }}
-  })
-    }
-    
+   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+   conn.sendFile(m.chat, global.API('vanessa, '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
-
 handler.command = /^attp$/i
-
 export default handler
